d29e57e3494ea3b43fc4078c098a0c6e62a6c256
jiwon <angry9908@gmail.com>
2025-04-15 16:31:50 +0900

    setting: project setting

---
+++ src/main/java/kr/ac/hansung/course/controller/CourseController.java
@@ -0,0 +1,19 @@
+package kr.ac.hansung.course.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/course")
+public class CourseController {
+
+    public String getCompletedCoursesByGrade(int studentId, int grade) {
+        return null;
+    }
+
+    public String registerCourse(int studentId, int grade) {
+        return null;
+    }
+
+    public String getRegisteredCourses() {
+        return null;
+    }
+}
